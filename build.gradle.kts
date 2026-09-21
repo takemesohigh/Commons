9bc19cd225a4bcc83be88f3114430ab47f78a299
Dahyun Park <howielyn23@gmail.com>
2026-09-22 01:08:06 +0800

    Update build.gradle.kts

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,10 +1,11 @@
 plugins {
     alias(libs.plugins.android).apply(false)
-    alias(libs.plugins.kotlinAndroid).apply(false)
+    alias(libs.plugins.kotlinAndroid).apply(false)    
     alias(libs.plugins.ksp).apply(false)
     alias(libs.plugins.parcelize).apply(false)
     alias(libs.plugins.library).apply(false)
     alias(libs.plugins.kotlinSerialization).apply(false)
+    alias(libs.plugins.compose.compiler).apply(false)
 }
 
 tasks.register<Delete>("clean") {
